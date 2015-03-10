50fc00f5bb82ba079ef747ab7c1b9f1eca8141ec
LamaCassie <chausse.cassie@gmail.com>
2015-03-10 15:56:44 +0100

    TEST PUBPSHY

--- Programme perl/BD/bd.pl
+++ Programme perl/BD/bd.pl
@@ -7,8 +7,8 @@ if ( !open(OUT, ">>FusionPP.txt") ) {
   exit(0);
 }
 
-if ( !open(IN, "test.txt") ) {
-	print " Erreur d'ouverture de test.txt \n"; 
+if ( !open(IN, "PP - Copie.txt") ) {
+	print " Erreur d'ouverture de PP - Copie.txt \n"; 
 	exit(0);	
 }
 $indent = 0;
@@ -18,13 +18,25 @@ while(<IN>) {
 	if($_ =~ /Résultat:/) {
 		$indent++;
 		print OUT ("------------------------------------\n");
-		print OUT ("PMID : $indent \n");
-	} elsif ($_ =~ /ID:/) {
-		print OUT ("$_ \n");
-	} elsif ($_ =~ /TI:/) {
-		print OUT ("$_ \n");
+		print OUT ("PMID- $indent \n");
+	} elsif ($_ =~ /TI:\s*(.*)/) {
+		print OUT ("TI - $1 \n");
+	} elsif ($_ =~ /AU:\s*(.*)/) {
+		print OUT ("AU - $1 \n");
+	} elsif ($_ =~ /PY:\s*(.*)/) {
+		print OUT ("PY - $1 \n");
+	} elsif ($_ =~ /LA:\s*(.*)/) {
+		print OUT ("LA - $1 \n");
+	} elsif ($_ =~ /ABHR:\s*(.*)/) {
+		print OUT ("AB - $1 \n");
 	} elsif ($_ =~ /CTEH:\s*(.*)/) {
-		print OUT ("CTECH : $1 \n");
+		print OUT ("MC - $1 \n");
+	} elsif ($_ =~ /CS:\s*(.*)/) {
+		print OUT ("AD - $1 \n");
+	} elsif ($_ =~ /COU:\s*(.*)/) {
+		print OUT ("PY - $1 \n");
+	} elsif ($_ =~ /EMAILO:\s*(.*)/) {
+		print OUT ("EM - $1 \n");
 	}
 }
 
@@ -32,29 +44,29 @@ close(OUT);
 close(IN);
 
 ####################### BD PQ #######################
-if ( !open(OUT, ">>FusionPQ.txt") ) {
-  print " Erreur d'ouverture de FusionPQ.txt \n"; 
-  exit(0);
-}
+# if ( !open(OUT, ">>FusionPQ.txt") ) {
+  # print " Erreur d'ouverture de FusionPQ.txt \n"; 
+  # exit(0);
+# }
 
-if ( !open(IN, "test.txt") ) {
-	print " Erreur d'ouverture de test.txt \n"; 
-	exit(0);	
-}
-$indent = 0;
-while(<IN>) {
-	#Incrémentation pour la clé primaire
-	#print OUT ("DEBUT\t- PU$indent \n");
-	#print OUT ("LIGNE : $_ \n");
-	if($_ =~ /Résumé:/) {
-		$indent++;
-		print OUT ("------------------------------------\n");
-		print OUT ("PMID : $indent \n");
-	} elsif ($_ =~ /Titre:/) {
-		print OUT ("$_ \n");
-	} elsif ($_ =~ /Lieu:/) {
-		print OUT ("$_ \n");
-	} elsif ($_ =~ /Langue:\s*(.*)/) {
-		print OUT ("CTECH : $1 \n");
-	}
-}
+# if ( !open(IN, "test.txt") ) {
+	# print " Erreur d'ouverture de test.txt \n"; 
+	# exit(0);	
+# }
+# $indent = 0;
+# while(<IN>) {
+	##Incrémentation pour la clé primaire
+	##print OUT ("DEBUT\t- PU$indent \n");
+	##print OUT ("LIGNE : $_ \n");
+	# if($_ =~ /Résumé:/) {
+		# $indent++;
+		# print OUT ("------------------------------------\n");
+		# print OUT ("PMID : $indent \n");
+	# } elsif ($_ =~ /Titre:/) {
+		# print OUT ("$_ \n");
+	# } elsif ($_ =~ /Lieu:/) {
+		# print OUT ("$_ \n");
+	# } elsif ($_ =~ /Langue:\s*(.*)/) {
+		# print OUT ("CTECH : $1 \n");
+	# }
+# }
