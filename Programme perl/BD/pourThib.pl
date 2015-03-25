ae2a9f366c8c7eebee69217aee64060a87774420
LamaCassie <chausse.cassie@gmail.com>
2015-03-25 16:07:11 +0100

    programme perl (ne pas tester)

--- Programme perl/BD/pourThib.pl
+++ Programme perl/BD/pourThib.pl
@@ -4,13 +4,13 @@
 #A la fin il suffit de regrouper les trois fichiers .txt réponses dans un seul
 
 ####################### BD PUBPSYCH #######################
-if ( !open(OUT, ">>POURTHIB.txt") ) {
-  print " Erreur d'ouverture de POURTHIB.txt \n"; 
+if ( !open(OUT, ">>POURTHIB2.txt") ) {
+  print " Erreur d'ouverture de POURTHIB2.txt \n"; 
   exit(0);
 }
 
-if ( !open(IN, "test.txt") ) {
-	print " Erreur d'ouverture de test.txt \n"; 
+if ( !open(IN, "testPubmed.txt") ) {
+	print " Erreur d'ouverture de testPubmed.txt \n"; 
 	exit(0);	
 }
 $MC = "";
@@ -18,6 +18,32 @@ $AB = "";
 $TI = "";
 $DA = "";
 @MC = ();
+# while(<IN>) {
+
+	# if($_ =~ /MC -(.*)/) {
+		# @MC[$indMC] = $1;
+		# $indMC++;
+	# } elsif ($_ =~ /AB -(.*)/) {
+		# $AB = $1;
+	# } elsif ($_ =~ /TI -(.*)/) {
+		# $TI = $1;
+	# } elsif ($_ =~ /DA -(.*)/) {
+		# $DA = $1;
+	# } elsif ($_ =~ /PMID-/) {
+		# print OUT ("DIC - $AB * $TI * ");
+		# foreach $MC (@MC) {
+			# print OUT ("$MC ");
+		# }
+		# print OUT ("\nDA - $DA \n");
+		# $MC = "";
+		# $AB = "";
+		# $TI = "";
+		# $DA = "";
+		# @MC = ();
+		# print OUT ("\n----------------------------------------------\n");
+	# }
+# }
+
 while(<IN>) {
 
 	if($_ =~ /MC -(.*)/) {
